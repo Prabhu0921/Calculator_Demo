1e2a36845c777111d1d76fe6965bbca00d2c22e9
Prabhu0921 <prabhugoudast@gmail.com>
2022-05-03 16:30:35 +0200

    Adding the Calculator initial code

---
+++ Calci.c
@@ -0,0 +1,9 @@
+//This program is to demonstarte the GIT using calculator program as an example.
+//#include <stdio.h> 
+
+int main()
+{
+    printf("Welcome to Calcualtor");
+    printf("Please select the operation:");
+return(0);
+}
