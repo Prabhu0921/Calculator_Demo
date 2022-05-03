ac00c8c96f3b9560e29f29deba2316c666aa98de
Prabhu0921 <prabhugoudast@gmail.com>
2022-05-03 16:59:01 +0200

    Added Multiplication Fun

--- Calci.c
+++ Calci.c
@@ -1,9 +1,12 @@
 //This program is to demonstarte the GIT using calculator program as an example.
-//#include <stdio.h> 
+#include <stdio.h> 
 
 int main()
 {
     printf("Welcome to Calcualtor");
     printf("Please select the operation:");
+    sum(a ,b); //new sum function
+    multiplication(a,b,c);
+
 return(0);
 }
